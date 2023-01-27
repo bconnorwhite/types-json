3227e8b99210006f108f080f22a78dcce76fe6a7
Connor White <connor.bcw@gmail.com>
2023-01-27 08:11:51 -0600

    feat: add JSONMagnitudinous

--- source/primitive/index.ts
+++ source/primitive/index.ts
@@ -6,6 +6,8 @@ import { numberSchema } from "./number.js";
 import { booleanSchema } from "./boolean.js";
 import { nullSchema } from "./null.js";
 
+// JSONPrimitive
+
 export type JSONPrimitive = string | number | boolean | null;
 export type OptionalJSONPrimitive = Optional<JSONPrimitive>;
 
@@ -21,6 +23,22 @@ export const isJSONPrimitive = is(jsonPrimitiveSchema);
 export const isOptionalJSONPrimitive = is(optionalJsonPrimitiveSchema);
 
 export const parseJSONPrimitive = parse(jsonPrimitiveSchema);
+
+// JSONMagnitudinous
+
+export type JSONMagnitudinous = string | number;
+export type OptionalJSONMagnitudinous = Optional<JSONMagnitudinous>;
+
+export const jsonMagnitudinousSchema: z.ZodSchema<JSONMagnitudinous> = z.union([
+  stringSchema,
+  numberSchema
+]);
+export const optionalJsonMagnitudinousSchema: z.ZodSchema<OptionalJSONMagnitudinous> = jsonMagnitudinousSchema.optional();
+
+export const isJSONMagnitudinous = is(jsonMagnitudinousSchema);
+export const isOptionalJSONMagnitudinous = is(optionalJsonMagnitudinousSchema);
+
+export const parseJSONMagnitudinous = parse(jsonMagnitudinousSchema);
 
 export * from "./string.js";
 export * from "./number.js";
